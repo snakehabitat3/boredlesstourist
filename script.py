5812c7b1c50a00f7d1d7cee125b442881a356a1d
awalm <85308324+snakehabitat3@users.noreply.github.com>
2023-07-01 23:18:30 +0900

    Add files via upload

---
+++ script.py
@@ -0,0 +1,3 @@
+destinations = ['Parsis, France', 'Shanghai, China', 'Los Angeles, USA', 'Sao Paulo, Brazil', 'Cairo, Egypt']
+
+test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
